1fab9905908fddc5e74d17543ff9ce5e5701461d
HugoMengualAlcover <hugome2307@gmail.com>
2023-02-02 09:14:38 +0100

    1

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Movie} from "../../common/movie";
 import {MovieService} from "../../services/movie.service";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-movie-list',
@@ -9,15 +10,45 @@ import {MovieService} from "../../services/movie.service";
 })
 
 export class MovieListComponent implements OnInit {
-// creamos el array donde guardaremos las películas
-// que traigamos de la API REST
+// creamos el array donde guardaremos las películas que traigamos de la API REST
   movies: Movie[] = [];
+
+  formMovie: FormGroup = this.formBuilder.group({
+    _id: [''],
+    __v: [0],
+    title: [''],
+    year: [2022],
+    director: [''],
+    plot: [''],
+    genres: [],
+    poster: [''],
+    imdb: this.formBuilder.group({
+      rating: [0],
+      votes: [0]
+    })
+  });
+
+  // Creamos el input para añadir un nuevo género
+  mynewGenre = new FormGroup({
+    newGenre: new FormControl('')
+  });
+
+// Creamos el array que guardara todos los géneros que tenemos en la base de datos
+  genres: string[] = [];
+
+// Creamos la variable editar para diferenciar
+// Editar de nueva película
+  editar = false;
+
 // inyectamos el servicio para poder utilizarlo
-  constructor(private movieService: MovieService) { }
-// en el ngOnInit es donde se ponen las
-// funciones que queremos ejecutar cuando se
-// inicie este componente
-// en nuestro caso queremos que se lean las películas
+// y el formBuilder para construir el formulario
+  constructor(private movieService: MovieService,
+              private formBuilder: FormBuilder) {
+
+  }
+
+// en el ngOnInit es donde se ponen las funciones que queremos ejecutar cuando se
+// inicie este componente en nuestro caso queremos que se lean las películas
   ngOnInit(): void {
     this.listMovies();
   }
@@ -26,17 +57,96 @@ export class MovieListComponent implements OnInit {
 // que la hemos llamado desde el ngOnInit
   listMovies(): void {
 
-// llamamos a la función del servicio que tiene
-// las películas y nos subscribimos a ella
-// Así nos mantiene la información actualizada
+    // Llamamos a la función del servicio que tiene las películas y
+    // nos subscribimos a ella. Así nos mantiene la información actualizada
     this.movieService.getMovieList().subscribe(
-// recogemos la respuesta de la subscripción
-// en una variable llamada data
+        //Recogemos la respuesta de la subscripción en una variable llamada data
       (data: any) => {
-// guardamos la respuesta en nuestro array de Movie
+        // Guardamos la respuesta en nuestro array de Movie
         this.movies = data;
+
+        // Añadimos la función que nos devuelve los géneros
+        this.movieService.getGenres().subscribe(
+          (data:any) => {
+            this.genres = data;
+          }
+        )
       }
     );
   }
 
+
+  // Función para cuando guarden el formulario
+  // que diferencia si están actualizando o añadiendo
+  onSubmit() {
+    // si estamos actualizando, llamamos a actualizar
+    if (this.editar) {
+      const id = this.formMovie.getRawValue()._id;
+
+      this.movieService.updateMovie(id,
+        this.formMovie.getRawValue()).subscribe(
+        (data: any) => {
+          this.listMovies();
+        }
+      );
+    } else { // si no, es que vamos a añadir una película nueva
+      this.movieService.newMovie(this.formMovie.getRawValue()).subscribe(
+        (data: any) => {
+          console.log(data);
+          this.listMovies();
+        }
+      )
+    }
+  }
+
+
+  // Función para cargar la película que vamos a actualizar
+  // Ponemos el boolean Editar a true
+  loadMovie(movie: Movie) {
+    this.formMovie.setValue(movie);
+    this.editar = true;
+  }
+
+  // Función para borrar el formulario para añadir una
+  // película nueva. Editar a falso
+  newMovie() {
+    this.formMovie.reset();
+    this.editar = false;
+  }
+
+  // Función para añadir un nuevo género a la lista
+  // Diferenciamos la lista de nueva película de la de actualizar
+  addNewGenre(newGenre: string) {
+    let newGenres;
+  // si es nueva película añadimos el género a nuestra lista de géneros de la base de datos
+    if (!this.editar) this.genres.push(newGenre)
+  // si no entonces tenemos que añadir el género
+  // a los géneros de la película que estamos actualizando
+
+    else {
+      // Guardo el array de géneros
+      newGenres = this.formMovie.getRawValue().genres;
+      // Añado el nuevo género al array
+      newGenres.push(newGenre);
+      // Actualizo el array de géneros en el formulario
+      this.formMovie.setControl(
+        'genres',
+        new FormControl(newGenres)
+      );
+    }
+    // para terminar borramos el campo del nuevo género
+    this.mynewGenre.reset();
+  }
+
+  // Función que borra una película
+  removeMovie(movie: Movie) {
+    if(confirm('Desea borrar '+movie.title+'?')) {
+      this.movieService.removeMovie(movie._id).subscribe(
+        data => {
+          console.log(data);
+          this.listMovies();
+        }
+      );
+    }
+  }
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -4,12 +4,28 @@ import {Movie} from "../common/movie";
 
 export class MovieService {
   baseURL = 'http://localhost:3000/api/movies';
-// Inyectamos el Cliente Http que nos
-// permitirá reealizar peticiones Http a la API REST
+  // Inyectamos el Cliente Http que nos permitirá reealizar peticiones Http a la API REST
   constructor(private http: HttpClient) { }
-// Creamos la función que nos devolverá
-// el array de Películas
+  // Creamos la función que nos devolverá el array de Películas
   getMovieList(): Observable<Movie[]> {
     return this.http.get<Movie[]>(this.baseURL);
+  }
+
+  // Función para actualizar una película
+  updateMovie(id: string, movie: Movie): Observable<any> {
+    return this.http.put(this.baseURL+'/'+id,movie);
+  }
+  // Función para cargar los géneros de la base de datos
+  getGenres(): Observable<string[]> {
+    return this.http.get<string[]>(this.baseURL+'/genres');
+  }
+  // Función para añadir una nueva película
+  newMovie(movie: Movie): Observable<any> {
+    return this.http.post(this.baseURL,movie);
+
+  }
+  // Función que borra una película
+  removeMovie(id: string): Observable<any> {
+    return this.http.delete(this.baseURL+'/'+id);
   }
 }
